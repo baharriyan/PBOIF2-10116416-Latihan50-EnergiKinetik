fb5e26769090dec3f9045ba9bb3012ba972e46ea
baharriyan <indonesiariyan@gmail.com>
2020-11-15 02:07:42 +0700

    first commit

---
+++ PBOIF2-10116416-Latihan50-energi/src/pboif2/pkg10116416/latihan50/energikinetik/PBOIF210116416Latihan50Energi.java
@@ -0,0 +1,17 @@
+package pboif2.pkg10116416.latihan50.energikinetik;
+/**
+ * @author
+ * Nama  : RIYAN BAHAR
+ * Kelas : PBOIF2
+ * NIM   : 10116416
+ * Deskripsi Program : Menghitung Energi Kinetik dan Usaha dalam suatu bola
+**/
+
+import Bola.BolaBaseball;
+public class PBOIF210116416Latihan50EnergiKinetik {
+    public static void main(String[] args) {       
+        BolaBaseball bola1 = new BolaBaseball(0.145, 25);
+        bola1.tampil();
+    }
+    
+}
